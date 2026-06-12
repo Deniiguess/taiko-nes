a57e51e5f7bca85ef33cc3c32ed23012ed3ccc37
Deni_iguess <denicentektheyosh@seznam.cz>
2026-06-12 12:46:57 +0200

    oh cool i introduced more issues (spawns randomly spawn)

--- code.asm
+++ code.asm
@@ -1256,8 +1256,6 @@ vblankwait2:
   STA $2000 ; PPUCTRL
   LDA #%00011110  ; turn on screen
   STA PPUMASK
-
-  JMP stay_here
 .endproc
 
 .proc stay_here
@@ -1279,10 +1277,10 @@ vblankwait2:
   RTS
 
 scenes_lo:
-.lobytes main_game, title_screen, song_sel
+.lobytes main_game, title_screen, song_sel, results
 
 scenes_hi:
-.hibytes main_game, title_screen, song_sel
+.hibytes main_game, title_screen, song_sel, results
 
 .endproc
 
@@ -1678,10 +1676,7 @@ scenes_hi:
   STA PPUSCROLL_X_speed
   STA PPUSCROLL_Y_speed
 
-  LDA #$02
-  STA scene
-
-  ; load PRG banks
+  ; load PRG banks (just in case)
   LDA #$01
   STA $E000
 
@@ -1707,10 +1702,6 @@ scenes_hi:
   STA $D000
   LDA #$0D
   STA $C000
-
-  ; hide sprite 0
-  LDA #$F0
-  STA $200
 
   LDA #$00
   STA fade_intensity
@@ -1803,6 +1794,158 @@ scenes_hi:
   JMP stay_here
 .endproc
 
+.proc load_results
+  LDA PPUMASK
+  AND #%11100111
+  STA PPUMASK
+
+  LDA PPUCTRL
+  STA PPUCTRL_kept
+  AND #%01111000
+  STA PPUCTRL
+
+  ; disable sprite 0 hit
+  LDA misc
+  AND #$FE
+  STA misc
+
+  ; unload all sprites
+  LDX #$00
+  unload_sprites:
+  LDA #$F0
+  STA $200, X
+  INX
+  INX
+  INX
+  INX
+  BNE unload_sprites
+
+  :
+  LDA misc
+  AND #$80
+  BNE :-
+
+  ; reset scrolling
+  LDA #$00
+  STA PPUSCROLL_X
+  STA PPUSCROLL_Y
+  STA PPUSCROLL_X_speed
+  STA PPUSCROLL_Y_speed
+
+  ; load PRG banks (just in case)
+  LDA #$01
+  STA $E000
+
+  ; load CHR banks
+  LDA #$00
+  STA $8000
+  LDA #$01
+  STA $8800
+  STA song_sel_entry
+  LDA #$02
+  STA $A000
+  LDA #$08
+  STA $A800
+  LDA #$09
+  STA $B000
+
+  ; load nametable banks
+  ; load PPU nametables
+  LDA #$E0
+  STA $C800
+  STA $D800
+  LDA #$E1
+  STA $D000
+  LDA #$0D
+  STA $C000
+
+  LDA #$00
+  STA fade_intensity
+
+  LDX #$00
+  STX ts_ss_timer
+  STX ts_ss_timer+1
+  loop_reset_palette:
+  LDA song_sel_pal, X
+  STA palette, X
+  INX
+  CPX #$20
+  BNE loop_reset_palette
+
+  ; load background
+  LDA PPUSTATUS
+  LDA #$20
+  STA PPUADDR
+  LDA #$00
+  STA PPUADDR
+  LDX #$00
+
+;   load_results_bg:
+;   load_results_1:
+;   LDA results_1, X
+;   STA PPUDATA
+;   INX
+;   BNE load_results_1
+;
+;   load_results_2:
+;   LDA results_2, X
+;   STA PPUDATA
+;   INX
+;   BNE load_results_2
+;
+;   load_results_3:
+;   LDA results_3, X
+;   STA PPUDATA
+;   INX
+;   BNE load_results_3
+;
+;   load_results_4:
+;   LDA results_4, X
+;   STA PPUDATA
+;   INX
+;   BNE load_results_4
+
+  LDA #$03
+  STA scene
+  LDA #$00
+  STA PPUSCROLL_X
+
+  LDA PPUMASK
+  ORA #%00011000
+  STA PPUMASK
+
+  :
+  BIT PPUSTATUS
+  BPL :-
+
+  LDA PPUCTRL
+  ORA #%10000100
+  STA PPUCTRL_kept
+  STA PPUCTRL
+  STA $2000
+
+  LDA #MBANKS_BANK
+  STA $F000
+  JSR init_song
+
+  LDA #$01
+  JSR famistudio_sfx_sample_play
+
+  LDA #$FF
+  STA song_sel_position+1
+
+  LDA #$04
+  STA fade_intensity
+
+  LDA #$00
+  STA fade_type
+
+  LDA #$01
+  STA fade_time
+
+  JMP stay_here
+.endproc
+
 init_song:
 ; define song
   LDX #<$C000 ; load low byte to X
@@ -1844,7 +1987,7 @@ taiko_sfx:
 
 
 
-.segment "TITL_SCEN_SONG_SEL"
+.segment "TITL_SCEN_SONG_SEL_RES"
 
 .proc title_screen
   NOP
@@ -2483,6 +2626,16 @@ song_sel_pal:
 
 controller_highlight_sprite_data:
   .byte $D9, $66, $00, $9E, $D9, $66, $00, $A5, $DB, $68, $00, $B4, $DB, $6A, $00, $C4
+
+.proc results
+  NOP
+  LDA BTN_Press
+  AND #BTN_A
+  BEQ :+
+  JMP load_song_sel
+  :
+  JMP stay_here
+.endproc
 
 
 .segment "MAIN_GAME"
@@ -4113,7 +4266,7 @@ controller_highlight_sprite_data:
   LDA end_song_timer
   BNE :+
 
-  JMP load_song_sel
+  JMP load_results
   :
 
   CMP #$09
